64b6078905848e68ac15f0cec4c52b2aafe7912e
AhmadM-DL <amm90@mail.aub.edu>
2020-07-25 12:06:23 +0300

    update

--- SEML/seml.py
+++ SEML/seml.py
@@ -12,7 +12,7 @@ import string
 
 se_conf_papers_w_abstract_fp = open("softwar_eng_confs_papers_with_abst.json")
 se_conf_papers_w_abstract = json.load(se_conf_papers_w_abstract_fp)
-se_conf_papers_w_abstract_df = pd.DataFrame(columns=["Id", "Conf", "Year", "Title", "Authors", "Abstract"])
+se_conf_papers_w_abstract_df = pd.DataFrame(columns=["Id", "Conference", "Year", "Title", "Authors", "Abstract"])
 
 # %%
 for conf, conf_obj in se_conf_papers_w_abstract.items():
@@ -42,28 +42,50 @@ ax= df2[['2017','2018', '2019']].plot(kind='bar', stacked=True)
 
 #%%
 
-# ml_keywords= ["train", "training", "learning", "classify", "classifier", "cluster", "clustering", "regression", "machine", "deep", "neural", "network", "networks"]
+ml_keywords= ["train", "training", "learning", "classify", "classifier", "cluster", "clustering", "regression", "machine", "deep", "neural", "network", "networks"]
 
 
 
-# scores = [0]*len(se_conf_papers_w_abstract_df)
-# for i in range(len(se_conf_papers_w_abstract_df)):
-#     row_ml_score=0
-#     for keyword in ml_keywords:
-#         if keyword in se_conf_papers_w_abstract_df["Title"][i] or keyword in se_conf_papers_w_abstract_df["Abstract"][i]:
-#             row_ml_score+=1
-#     scores[i]=row_ml_score
+scores = [0]*len(se_conf_papers_w_abstract_df)
+for i in range(len(se_conf_papers_w_abstract_df)):
+    row_ml_score=0
+    for keyword in ml_keywords:
+        if keyword in se_conf_papers_w_abstract_df["Title"][i] or keyword in se_conf_papers_w_abstract_df["Abstract"][i]:
+            row_ml_score+=1
+    scores[i]=row_ml_score
 
-# se_conf_papers_w_abstract_df["ml_score"] = scores
+se_conf_papers_w_abstract_df["ml_score"] = scores
 
-# sorted_by_ml_score = se_conf_papers_w_abstract_df.sort_values(by="ml_score", ascending=False)
+# %%
+se_conf_papers_w_abstract_df[se_conf_papers_w_abstract_df["ml_score"] == 1].to_csv("temp.csv")
 
-# sorted_by_ml_score_100 = sorted_by_ml_score.head(100)
+# %%
+se_conf_papers_w_abstract_df.groupby(["ml_score"])["Id"].count()
 
-#######
+# %%
+se_conf_papers_w_abstract_df["ml_score"].hist()
 
-sorted_by_ml_score_100 = pd.read_csv("C:/Users/PC/Desktop/Interaction SE with ML/SEML/top100.csv")
-grouped = sorted_by_ml_score_100.groupby(by="Conf")
+# %%
+sorted_by_ml_score = se_conf_papers_w_abstract_df.sort_values(by="ml_score", ascending=False)
+sorted_by_ml_score_100 = sorted_by_ml_score.head(100)
+
+# %%
+sorted_by_ml_score_100.groupby(by="Conference").count()["Id"].plot.bar()
+plt.title("Studied SE-ML papers count distribution over conferences")
+plt.ylabel("Counts")
 
 
+# %%
 
+_all_ = se_conf_papers_w_abstract_df.groupby(['Conference'])['Id'].count()
+_studied_ = sorted_by_ml_score_100.groupby(by="Conference")["Id"].count()
+
+(_studied_/_all_*100).plot.bar()
+plt.title("Percentage of machine learning papers in conferences")
+plt.ylabel("%")
+
+# %%
+(_studied_/_all_*100).mean()
+
+
+# %%
---
+++ SEML/top100.py
@@ -0,0 +1,11 @@
+# %%
+
+from collections import defaultdict
+import json
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import re
+import string
+
+# %%
